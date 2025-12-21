8f070d50f0546f0d55539ca269e7f05a67f4d402
KovDan <koval_dan2004@mail.ru>
2025-12-21 15:16:38 +0700

    Добавлен JWT для авторизации и аутентификации.

---
+++ backend/Services/JwtService.cs
@@ -0,0 +1,43 @@
+﻿using backend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace backend.Services
+{
+    public class JwtService
+    {
+
+        private readonly IConfiguration configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var jwtSettings = configuration.GetSection("Jwt");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+            var claims = new[]
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.Name)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(jwtSettings["DurationInMinutes"])),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
